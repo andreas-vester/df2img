028bd858d4a8d642becce08ec356ff020741053f
Andreas Vester <andreas@vester-online.de>
2022-08-29 22:24:12 +0200

    fix: Python version in ``noxfile.py``

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.10", "3.9", "3,8"])
+@nox.session(python=["3.10.6", "3.9", "3.8"])
 def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", "-E", "docs", external=True)
